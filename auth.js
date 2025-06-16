38b9bf6a94b483265e981b31431ba0b9cac99c18
Your Full Name <your-email@example.com>
2025-06-16 16:07:24 +0530

    removed backend stuff

--- auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Signup, login } from './authcontroller.js';
-const router = express.Router();
-
-// Define signup and login routes with correct path syntax
-router.post("/api/signup", Signup);
-router.post("/api/login", login);
-
-export default router;
